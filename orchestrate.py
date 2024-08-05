ccf1e827fcb168ae02ad8dae52d0105fff94bf77
Abdelrahman Elsharkawi <abdelrahman.elsharkawi@student.htw-berlin.de>
2024-08-05 15:22:49 +0200

    edit writing results

--- orchestrate.py
+++ orchestrate.py
@@ -6,9 +6,9 @@ import pandas as pd
 def run_experiment_in_env(env_name, output_path, time_limit):
     return subprocess.Popen(['bash', 'run_experiment.sh', env_name, str(output_path), str(time_limit)])
 
-def update_results(path, time_limit):
+def update_results(path, time_limit, framework):
     results = pd.read_csv(path / f"results_{time_limit}.csv")
-    aggregated_results_file = Path('results') / f'aggregated_results_{time_limit}.csv'
+    aggregated_results_file = path.parent / f'aggregated_results_{time_limit}.csv'
     
     if aggregated_results_file.exists():
         aggregated_results = pd.read_csv(aggregated_results_file)
@@ -17,7 +17,7 @@ def update_results(path, time_limit):
         aggregated_results.set_index(['PMLB dataset', 'Logistic Regression'], inplace=True)
         results.set_index(['PMLB dataset', 'Logistic Regression'], inplace=True)
 
-        aggregated_results.update(results[['tabpfn']])
+        aggregated_results.update(results[framework])
 
         # Reset index if necessary
         aggregated_results.reset_index(inplace=True)
@@ -28,22 +28,22 @@ def update_results(path, time_limit):
     aggregated_results.to_csv(aggregated_results_file, index=False)
 
 def main():
-    working_directory = Path.cwd() / "workdir"
+    datetime_str = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    working_directory = Path.cwd() / "workdir" / f"Exp_{datetime_str}"
     frameworks = ['tabpfn', 'hyperfast']
     processes = []
     time_limits = [60, 120]
-    datetime_str = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
 
     for time_limit in time_limits:
         for env in frameworks:
-            output_path = working_directory / f"Exp{datetime_str}_{env}"
+            output_path = working_directory / env
             output_path.mkdir(parents=True, exist_ok=True)
             process = run_experiment_in_env(env, output_path, time_limit)
             processes.append((process, env, output_path, time_limit))
         
         for process, env, output_path, time_limit in processes:
             process.wait()
-            update_results(output_path, time_limit)
+            update_results(output_path, time_limit, env)
     
     print("All experiments completed and results updated.")
 
